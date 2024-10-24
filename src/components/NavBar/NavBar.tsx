7ad582ea9badd40ca323ccda59d0db358eee0c23
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-10-24 02:54:27 -0300

    feat: mobile navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import './NavBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="navbar">
-      <ul className="navbar-links">
-        <li>
-          <a href="#sobre-mim" className="nav-link">Sobre mim</a>
-        </li>
-        <li>
-          <a href="#skills" className="nav-link">Skills</a>
-        </li>
-        <li>
-          <a href="#projetos" className="nav-link">Projetos</a>
-        </li>
-        <li>
-          <a href="#eventos" className="nav-link">Eventos</a>
-        </li>
-        <li>
-          <a href="#contato" className="nav-link">Contato</a>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <div className="social-icons">
+          <a href="https://www.linkedin.com/in/seu-perfil" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} className="nav-icon" />
+          </a>
+          <a href="https://github.com/seu-perfil" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} className="nav-icon" />
+          </a>
+        </div>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul className="navbar-nav navbar-links ms-auto">
+            <li>
+              <a className="nav-link" href="#sobre-mim">Sobre mim</a>
+            </li>
+            <li>
+              <a className="nav-link" href="#skills">Skills</a>
+            </li>
+            <li>
+              <a className="nav-link" href="#projetos">Projetos</a>
+            </li>
+            <li>
+              <a className="nav-link" href="#eventos">Eventos</a>
+            </li>
+            <li>
+              <a className="nav-link" href="#contato">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
