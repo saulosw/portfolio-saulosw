cf9e701e83e5b3e72423a42d97bd63e45c8bf100
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-10-26 17:01:43 -0300

    feat: more project cards

--- src/components/ProjectSection/ProjectSection.tsx
+++ src/components/ProjectSection/ProjectSection.tsx
@@ -12,10 +12,24 @@ const ProjectsSection: React.FC = () => {
   const projects = [
     {
       title: "FoodPath",
-      description: "FoodPath é uma plataforma que conscientiza e facilita doações para combater a fome no Brasil. Através de estatísticas e parcerias com ONGs, o site conecta usuários a oportunidades de doação, promovendo uma distribuição justa de recursos para apoiar a erradicação da fome no país.",
+      description: "FoodPath é uma plataforma desenvolvida como projeto da faculdade para conscientizar e facilitar doações no combate à fome no Brasil. Com dados estatísticos e acesso a ONGs, o site conecta usuários a oportunidades de doação, promovendo uma distribuição de recursos para apoiar a erradicação da fome no país.",
       tecnologies: "React - JavaScript - HTML5 - CSS3 - Node.js - SQL",
       image: "https://placehold.co/600x400",
       githubLink: "https://github.com/saulosw/foodpath-FECAP"
+    },
+    {
+      title: "Bank Account Management",
+      description: "Sistema de gerenciamento de contas bancárias desenvolvido em Python. Permite operações essenciais como criar clientes e contas, realizar depósitos, saques e monitorar o histórico de transações. O sistema também envia e-mails de boas-vindas aos novos clientes com um código de validação.",
+      tecnologies: "Python - HTML5 - SQL",
+      image: "https://placehold.co/600x400",
+      githubLink: "https://github.com/saulosw/account-management"
+    },
+    {
+      title: "UnderOcean",
+      description: "UnderOcean é um jogo educacional desenvolvido em Unity proposto pela faculdade, para conscientizar sobre a preservação dos oceanos e da vida marinha. Nele, os jogadores têm a missão de remover poluentes do ambiente marinho, aprendendo sobre a preservação dos ecossistemas aquáticos.",
+      tecnologies: "C# - Unity",
+      image: "https://placehold.co/600x400",
+      githubLink: "https://github.com/saulosw/underocean_-main-game"
     },
   ];
 
